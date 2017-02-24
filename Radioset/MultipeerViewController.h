9fc945d467c9f9fcf979555b5a7e02456dea6deb
Kvachan <developer@iMac-Kvachan.local>
2017-02-24 11:13:05 +0300

    - fix NSStreamDelegate

--- Radioset/MultipeerViewController.h
+++ Radioset/MultipeerViewController.h
@@ -11,7 +11,7 @@
 
 @class MCBrowserViewController;
 
-@interface MultipeerViewController : ViewController <NSStreamDelegate>
+@interface MultipeerViewController : ViewController
 
 @property (nonatomic, readonly) MCBrowserViewController *browserViewController;
 
