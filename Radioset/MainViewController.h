c0fec9bb35c05b65d176f9e5edd3ac046315093a
KvachanPavel <pavelkvacan@MacBook-Pro-Pavel.local>
2017-05-09 20:02:30 +0300

    - add sidebar

---
+++ Radioset/MainViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainViewController.h
+//  Radioset
+//
+//  Created by Павел Квачан on 5/9/17.
+//  Copyright © 2017 Павел Квачан. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
+
+
+@end
--- Radioset/ViewController.mm
+++ Radioset/ViewController.mm
@@ -9,12 +9,12 @@
 #import "ViewController.h"
 #import "Recorder.h"
 #import "Player.h"
-#import "MultipeerViewController.h"
+//#import "MultipeerViewController.h"
 
 
 @interface ViewController ()
 {
-    MultipeerViewController *_multipeerViewController;
+//    MultipeerViewController *_multipeerViewController;
 }
 
 @end
@@ -25,7 +25,7 @@
 {
     [super viewDidLoad];
     
-    _multipeerViewController = [[MultipeerViewController alloc] init];
+//    _multipeerViewController = [[MultipeerViewController alloc] init];
 }
 - (IBAction)Start:(id)sender
 {
@@ -36,31 +36,31 @@
 //    data = [NSData dataWithBytes:[Recorder sharedRecorder].buffer.mData length:[Recorder sharedRecorder].buffer.mDataByteSize];
     
     
-    if (_multipeerViewController != nil)
-    {
-        while ([Recorder sharedRecorder].buffer.mDataByteSize > 0)
-        {
-        
-        if (_multipeerViewController.stream != nil)
-        {
-            void *testBuf = (void *)malloc([Recorder sharedRecorder].buffer.mDataByteSize);
-            memcpy(testBuf, [Recorder sharedRecorder].buffer.mData, [Recorder sharedRecorder].buffer.mDataByteSize);
-            
-            
-//             = static_cast<uint8_t *>(malloc([Recorder sharedRecorder].buffer.mDataByteSize *  sizeof(uint8_t)));
-//            uint8_t *buffer = static_cast<uint8_t *>([Recorder sharedRecorder].buffer.mData);
-            
-            
-
-            [_multipeerViewController.stream write:(uint8_t *)testBuf maxLength:[Recorder sharedRecorder].buffer.mDataByteSize];
-            
-        }
-        else
-        {
-            NSLog(@"qwe");
-        }
-        }
-    }
+//    if (_multipeerViewController != nil)
+//    {
+//        while ([Recorder sharedRecorder].buffer.mDataByteSize > 0)
+//        {
+//        
+//        if (_multipeerViewController.stream != nil)
+//        {
+//            void *testBuf = (void *)malloc([Recorder sharedRecorder].buffer.mDataByteSize);
+//            memcpy(testBuf, [Recorder sharedRecorder].buffer.mData, [Recorder sharedRecorder].buffer.mDataByteSize);
+//            
+//            
+////             = static_cast<uint8_t *>(malloc([Recorder sharedRecorder].buffer.mDataByteSize *  sizeof(uint8_t)));
+////            uint8_t *buffer = static_cast<uint8_t *>([Recorder sharedRecorder].buffer.mData);
+//            
+//            
+//
+//            [_multipeerViewController.stream write:(uint8_t *)testBuf maxLength:[Recorder sharedRecorder].buffer.mDataByteSize];
+//            
+//        }
+//        else
+//        {
+//            NSLog(@"qwe");
+//        }
+//        }
+//    }
 }
 
 - (IBAction)Stop:(id)sender
@@ -72,9 +72,9 @@
 
 - (IBAction)ShowBrowser:(id)sender
 {
-    [self presentViewController:(UIViewController *)_multipeerViewController.browserViewController
-                       animated:YES
-                     completion:nil];
+//    [self presentViewController:(UIViewController *)_multipeerViewController.browserViewController
+//                       animated:YES
+//                     completion:nil];
 
 }
 
